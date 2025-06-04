298c4fc6fb93549e964924f60b87d62f58c096fc
vincmgn <vincentmagnienpro@gmail.com>
2025-06-04 15:44:30 +0200

    refactor: improve error handling and simplify event listener

--- index.js
+++ index.js
@@ -6,17 +6,21 @@
 // Add users instead of replacing them.
 
 const fetchBtn = document.getElementById("fetch-users");
-const table = document.getElementById("tbl-users");
 const tblBody = document.querySelector("#tbl-users tbody");
 let users = []
 
 async function fetchUsers() {
-    const res = await fetch("https://randomuser.me/api/?results=20");
-    const data = await res.json();
-    return data.results;
+    try {
+        const res = await fetch("https://randomuser.me/api/?results=20");
+        const data = await res.json();
+        return data.results;
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        return [];
+    }
 }
 
-function renderUsers(users) {
+const renderUsers = (users) => {
     tblBody.innerHTML = "";
     tblBody.innerHTML = users.map((user) => {
         return `<tr>
@@ -25,16 +29,11 @@ function renderUsers(users) {
             <td>${user.email}</td>
             <td>${user.phone}</td>
             <td>${user.dob.age}</td>
-            <tr>
-            `
+        </tr>`;
     }).join("");
 }
 
 fetchBtn.addEventListener("click", async () => {
-    try {
-        users = await fetchUsers();
-        renderUsers(users);
-    } catch (error) {
-        console.error("Error fetching users:", error);
-    }
+    users = await fetchUsers();
+    renderUsers(users);
 });
