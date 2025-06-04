9f00f94ecb3c2c08d39a2a8a8e633477f5920b7d
vincmgn <vincentmagnienpro@gmail.com>
2025-06-04 16:33:49 +0200

    feat: implement gender filtering and display total users in user table

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Fill the table with https://randomuser.me/api/?results=20 when clicking the `fetch-users` button
-// Add age column
+// Add Age column
 // Add gender column, with two different emojis
+
 // Sort user by age
 // Search user by name
 // Add users instead of replacing them.
 
 const fetchBtn = document.getElementById("fetch-users");
 const tblBody = document.querySelector("#tbl-users tbody");
+const displayTotal = document.getElementById("display-total");
+const options = document.getElementById("options");
+const genderSelect = document.getElementById("gender-select");
 let users = []
 
 const fetchUsers = async () => {
@@ -19,6 +23,10 @@ const fetchUsers = async () => {
         return [];
     }
 }
+
+const displayTotalUsers = (displayed, total) => {
+    displayTotal.textContent = `${displayed} / ${total} users`;
+};
 
 const renderUsers = (users) => {
     tblBody.innerHTML = "";
@@ -34,7 +42,24 @@ const renderUsers = (users) => {
     }).join("");
 }
 
-fetchBtn.addEventListener("click", async () => {
+const displayUsers = async () => {
     users = await fetchUsers();
     renderUsers(users);
-});
+    displayTotalUsers(users.length, users.length);
+    options.style.display = "flex";
+}
+
+fetchBtn.addEventListener("click", displayUsers);
+
+// Filter user by gender
+const filterUsersByGender = () => {
+    const selectedGender = genderSelect.value.toLowerCase();
+    let filteredUsers = users;
+    if (selectedGender !== "none") {
+        filteredUsers = users.filter(user => user.gender === selectedGender);
+    }
+    renderUsers(filteredUsers);
+    displayTotalUsers(filteredUsers.length, users.length);
+}
+
+genderSelect.addEventListener("change", filterUsersByGender);
